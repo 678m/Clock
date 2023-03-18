90ca4c5063956119fee48c085ba779d64db4ce61
Tangang Mc Bride Tabufor <tangangbride@gmail.com>
2023-03-18 13:03:31 +0100

    Add dynamic seconds hand rotation to clock

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const sH = document.querySelector("#sH");
 const mH = document.querySelector("#mH");
+const hH = document.querySelector("#hH");
 
 const deg = 6;
 setInterval(() => {
   let day = new Date();
   let milliSec = day.getMilliseconds() * deg;
   let min = day.getMinutes() * deg;
+    let hour = day.getHours() * 30;
   let sec = day.getSeconds() * deg + milliSec / 1000;
   sH.style.transform = `rotateZ(${sec}deg)`;
   mH.style.transform = `rotateZ(${min}deg)`;
+    hH.style.transform = `rotateZ(${hour + min / 12}deg)`;
 });
