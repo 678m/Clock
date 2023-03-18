93ac031d4919df3e1e780326fb897ffa6925b096
Tangang Mc Bride Tabufor <tangangbride@gmail.com>
2023-03-18 12:50:50 +0100

    Add dynamic minute hand rotation to clock

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const sH = document.querySelector("#sH");
+const mH = document.querySelector("#mH");
 
 const deg = 6;
 setInterval(() => {
   let day = new Date();
   let milliSec = day.getMilliseconds() * deg;
+  let min = day.getMinutes() * deg;
   let sec = day.getSeconds() * deg + milliSec / 1000;
   sH.style.transform = `rotateZ(${sec}deg)`;
+  mH.style.transform = `rotateZ(${min}deg)`;
 });
